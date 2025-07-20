2613c6f3326375659f96e5d0afc4ca95de889c36
dancun48 <111135378+dancun48@users.noreply.github.com>
2025-07-20 14:49:26 +0300

    Add files via upload

---
+++ showhide.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btn');
+const img = document.getElementById('img');
+
+btn.addEventListener('click', (event) => {
+
+    if(img.style.visibility === 'hidden'){
+        img.style.visibility = 'visible';
+        btn.textContent = 'Hide';
+
+    }
+    else{
+        img.style.visibility = 'hidden';
+        btn.textContent = 'Show';
+    }
+})
